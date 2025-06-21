ede297056cd04878efa30d8a94b1fff2437a4c33
Khalil Johnson <khalil786.johnson@gmail.com>
2025-06-20 20:45:26 -0400

    setup repo

---
+++ lib/iex_debug.ex
@@ -0,0 +1,18 @@
+defmodule IexDebug do
+  @moduledoc """
+  Documentation for `IexDebug`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IexDebug.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/iex_debug_test.exs
@@ -0,0 +1,8 @@
+defmodule IexDebugTest do
+  use ExUnit.Case
+  doctest IexDebug
+
+  test "greets the world" do
+    assert IexDebug.hello() == :world
+  end
+end
